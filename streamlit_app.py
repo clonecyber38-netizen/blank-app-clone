192e2b5651cc657339b4c81ada0e757e1ef26470
clonecyber38-netizen <clonecyber38@gmail.com>
2026-05-19 11:16:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,17 @@ st.title("🎈 MAGALI BELOM MANDI")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.markdown("*FIKRI* is **really** ***cool***.")
+st.markdown('''
+    :red[FIKRI] :orange[can] :green[DO] :blue[ALL] :violet[ACTIVITY]
+    :gray[SO] :rainbow[EASY] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet FOR FIKRI &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''Be yourself; and never surrender
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
